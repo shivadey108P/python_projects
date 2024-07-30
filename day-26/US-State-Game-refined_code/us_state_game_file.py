9b46307538cdf6d3fefe9bf84481c2315cb17454
shivadey108P <shivadey108@gmail.com>
2024-07-30 23:37:25 +0530

    added day-26 folder

---
+++ day-26/US-State-Game-refined_code/us_state_game_file.py
@@ -0,0 +1,38 @@
+import turtle
+import pandas as pd
+
+screen = turtle.Screen()
+screen.title("U.S. State Game")
+image = "./day-25/US-State-Game/blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+guessed_states = []
+data = pd.read_csv('./day-25/US-State-Game/50_states.csv')
+all_states =  data.state.to_list()
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state name? ").title()
+    
+    if answer_state == 'Exit':
+        states_not_in_guessed = []
+        for state in all_states:
+            if state not in guessed_states:
+                states_not_in_guessed.append(state)
+
+        dict_learn_state = {
+            'State': states_not_in_guessed
+        }
+
+        df = pd.DataFrame(dict_learn_state)
+        df.to_csv('./day-25/US-State-Game/states_to_learn.csv')
+        break
+    if answer_state in all_states and answer_state not in guessed_states:
+        guessed_states.append(answer_state)
+        s = turtle.Turtle()
+        s.hideturtle()
+        s.penup()
+        state_data = data[data.state == answer_state]
+        s.goto(state_data.x.item(), state_data.y.item())
+        s.write(state_data.state.item())
+
+

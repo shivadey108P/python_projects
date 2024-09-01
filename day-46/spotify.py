d051cc9604e64f9062f1f3591cba4bbc21a3298c
shivadey108P <shivadey108@gmail.com>
2024-09-01 22:19:15 +0530

    added spotify code

--- day-46/spotify.py
+++ day-46/spotify.py
@@ -1,2 +1,20 @@
-import spotify
-from spotify.oauth2 import SpotifyClientCredentials
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SPOTIFY_ID = os.environ['SPOTIFY_CLIENT_ID']
+SPOTIFY_SECRET_KEY = os.environ['SPOTIFY_CLIENT_SECRET']
+BASE_URL = 'https://api.spotify.com.'
+
+class Spotify_Setup:
+    def __init__(self) -> None:
+        self.sp = spotipy.Spotify(
+            auth_manager=SpotifyOAuth(
+                client_id=SPOTIFY_ID,
+                client_secret=SPOTIFY_SECRET_KEY,
+                redirect_uri='https://open.spotifywithshivadey.com/',
+                scope='playlist-modify-private',
+                show_dialog=True))                               
